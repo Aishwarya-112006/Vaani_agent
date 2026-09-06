fb0cfb55fd6ca014b3921ba8220e911224d60668
Utkarsh Verma <utkarsh.sahil04@gmail.com>
2026-09-07 02:40:59 +0530

    search _restaurants #21

--- backend/tools/__init__.py
+++ backend/tools/__init__.py
@@ -1,3 +1,9 @@
 from .hotel_search import parse_hotel_params, search_hotels
+from .restaurant_search import parse_restaurant_params, search_restaurants
 
-__all__ = ["search_hotels", "parse_hotel_params"]
+__all__ = [
+    "search_hotels",
+    "parse_hotel_params",
+    "search_restaurants",
+    "parse_restaurant_params",
+]
